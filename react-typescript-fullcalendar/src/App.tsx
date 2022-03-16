7d620e44400f51b529dd8aafd619bab5f465cd93
kyohei ichimura <kyohei.ichimura@loioz.co.jp>
2022-03-16 14:38:44 +0900

    feat:クリックイベントの追加

--- react-typescript-fullcalendar/src/App.tsx
+++ react-typescript-fullcalendar/src/App.tsx
@@ -1,25 +1,32 @@
+import { useCallback } from "react";
+
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; 
 import allLocales from '@fullcalendar/core/locales-all';
 
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
 const thisMonth = () => {
   const today = new Date();
   return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
     2,
-    "0")}`
+    "0"
+  )}`;
 };
-
 function App() {
+  const handleDateClick = useCallback((arg : DateClickArg) => {
+    alert(arg.dateStr);
+  },[]);
   return (
     <div>
-        <FullCalendar plugins={[dayGridPlugin]} 
+        <FullCalendar plugins={[dayGridPlugin, interactionPlugin]} 
           initialView="dayGridMonth"
           locales={allLocales}
           locale="ja"
           events={[
-            {title:"event1",date:`${thisMonth()}-01` },
-            {title:"event2",date:`${thisMonth()}-02` },
+            { title: "event 1", date: `${thisMonth()}-01` },
+            { title: "event 2", date: `${thisMonth()}-02` },
           ]}
+          dateClick={handleDateClick}
         />
     </div>
   );
