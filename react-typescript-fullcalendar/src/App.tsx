31988d6ffbbdcbd1c984a37e9f9f30c1dbf9a879
kyohei ichimura <kyohei.ichimura@loioz.co.jp>
2022-03-16 14:07:51 +0900

    feat:カレンダーの日本語化

--- react-typescript-fullcalendar/src/App.tsx
+++ react-typescript-fullcalendar/src/App.tsx
@@ -1,10 +1,11 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; 
+import allLocales from '@fullcalendar/core/locales-all';
 
 function App() {
   return (
     <div>
-        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth"/>
+        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" locales={allLocales} locale="ja"/>
     </div>
   );
 }
