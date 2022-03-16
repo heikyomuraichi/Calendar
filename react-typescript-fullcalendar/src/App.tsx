7169c476b0260c338b4cefb9ebda1fe931b815e8
kyohei ichimura <kyohei.ichimura@loioz.co.jp>
2022-03-16 14:18:26 +0900

    feat:予定の追加（手動）

--- react-typescript-fullcalendar/src/App.tsx
+++ react-typescript-fullcalendar/src/App.tsx
@@ -2,10 +2,25 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; 
 import allLocales from '@fullcalendar/core/locales-all';
 
+const thisMonth = () => {
+  const today = new Date();
+  return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
+    2,
+    "0")}`
+};
+
 function App() {
   return (
     <div>
-        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" locales={allLocales} locale="ja"/>
+        <FullCalendar plugins={[dayGridPlugin]} 
+          initialView="dayGridMonth"
+          locales={allLocales}
+          locale="ja"
+          events={[
+            {title:"event1",date:`${thisMonth()}-01` },
+            {title:"event2",date:`${thisMonth()}-02` },
+          ]}
+        />
     </div>
   );
 }
